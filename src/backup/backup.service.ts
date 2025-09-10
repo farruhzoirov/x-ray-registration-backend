2bdd5a7bbf046793261ce7658257ca8c0c9e28a2
farruhzoirov <fzoirov29@gmail.com>
2025-09-10 12:49:17 +0500

    commit

--- src/backup/backup.service.ts
+++ src/backup/backup.service.ts
@@ -9,17 +9,14 @@ const execAsync = promisify(exec);
 
 @Injectable()
 export class BackupService {
-  private bot = new TelegramBot(
-    '8275777854:AAHyyIylq2G0Q0I_5uSRF6W2Dzruh3KEk38',
-    { polling: false },
-  );
-  private chatId = 8189683531;
+  private bot = new TelegramBot('', { polling: false });
+  private chatId = '';
   @Cron('55 22 * * *')
   async handleCron() {
     try {
       const fileName = `backup-${new Date().toISOString().split('T')[0]}.csv`;
       const filePath = `./${fileName}`;
-      const command = `mongoexport --uri="${'mongodb://superAdmin:Xf93%21qLz8%40VnRp2%23@62.171.170.212:27017/dynamics-market?authSource=admin'}" --collection=products --type=csv --fields="_id,nameUz,nameRu,nameEn" --out="${filePath}"`;
+      const command = `mongoexport --uri="${''}" --collection=products --type=csv --fields="_id,nameUz,nameRu,nameEn" --out="${filePath}"`;
       await execAsync(command);
       await this.bot.sendDocument(this.chatId, fs.createReadStream(filePath));
       console.log('✅ Backup done and sent to Telegram!');
