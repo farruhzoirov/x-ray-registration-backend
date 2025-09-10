2fdc47744e77011c27d83dd38b031ad4c041dc0b
farruhzoirov <fzoirov29@gmail.com>
2025-09-10 13:13:47 +0500

    commit

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,13 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const PORT = 7000;
 
+  app.enableCors({
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    allowedHeaders:
+      'Content-Type, Authorization, Accept-Language, App-Type, Accept',
+  });
+
   // app.useGlobalPipes(
   //   new ValidationPipe({
   //     whitelist: true,
