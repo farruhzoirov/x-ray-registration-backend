9075eb31fb51ac5bf233e0c03539a3791c4b8203
farruhzoirov <fzoirov29@gmail.com>
2025-09-13 23:54:40 +0500

    commit

--- src/backup/backup.service.ts
+++ src/backup/backup.service.ts
@@ -32,7 +32,7 @@ export class BackupService {
     ];
   }
 
-  @Cron('50 23 * * *')
+  @Cron('58 20 * * *')
   async handleCron() {
     const fileName = `backup-${new Date().toISOString().split('T')[0]}.csv`;
     const filePath = path.join(__dirname, '../../backups', fileName);
