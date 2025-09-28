21073b27d33309ece272cab435fcf62adb19764d
farruhzoirov <fzoirov29@gmail.com>
2025-09-28 15:49:31 +0500

    commit

--- src/helpers/generateWordFIle.helper.ts
+++ src/helpers/generateWordFIle.helper.ts
@@ -259,7 +259,16 @@ export const generatePdfFile = async (registrations: any[]) => {
   try {
     const browser = await puppeteer.launch({
       headless: true,
-      args: ['--no-sandbox', '--disable-setuid-sandbox']
+      args: [
+        '--no-sandbox',
+        '--disable-setuid-sandbox',
+        '--disable-dev-shm-usage',
+        '--disable-accelerated-2d-canvas',
+        '--no-first-run',
+        '--no-zygote',
+        '--single-process',
+        '--disable-gpu'
+      ]
     });
 
     const page = await browser.newPage();
